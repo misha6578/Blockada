642f434fa92e201b9d7d18f0c7c7006160c1888c
Nikita3421 <112571677+Nikita3421@users.noreply.github.com>
2022-09-11 16:26:26 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+from pygame import *
+win_width = 1280
+win_height = 720
+window = display.set_mode((win_width, win_height))
+display.set_caption("Maze_defection")
+background = transform.scale(image.load("Fone.jpg"), (win_width, win_height))
+window.blit(background,(0,0))
+
+while game:
+
+    display.update()
+    clock.tick(FPS)  
