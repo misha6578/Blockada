346da0d026f2f5c881dd12f187f4f8380916983a
tpashen <81863630+tpashen@users.noreply.github.com>
2022-09-18 15:15:21 +0300

    Update Untitled-1.py
    Создала переменные game., clock

--- Untitled-1.py
+++ Untitled-1.py
@@ -5,7 +5,8 @@ window = display.set_mode((win_width, win_height))
 display.set_caption("Maze_defection")
 background = transform.scale(image.load("Fone.jpg"), (win_width, win_height))
 window.blit(background,(0,0))
-
+clock = time.Clock()
+game = True
 while game:
     for e in event.get():
         if e.type == QUIT:
