a53aaf1fb280dc79ccd7242e17747c88e23b5ef1
Nikita3421 <112571677+Nikita3421@users.noreply.github.com>
2022-09-18 15:56:48 +0200

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -13,4 +13,33 @@ while game:
         if e.type == QUIT:
             game = False
     display.update()
-    clock.tick(FPS)  
+    clock.tick(FPS) 
+    
+    
+    #тут класс герой
+    class Player(Settings):
+
+    def r_l(self):
+        global mana, img, f
+        f=1
+        keys = key.get_pressed()
+        if keys[K_a]:
+            self.rect.x -= self.speed
+            mana.side = "left"
+            f = 1
+        if keys[K_d]:
+            self.rect.x += self.speed
+            f = 0
+            mana.side = "right"
+
+        if f == 1:
+            self.image = transform.scale(image.load(hero_r), (self.width, self.height))
+        if f == 0:
+            self.image = transform.scale(image.load(hero_l), (self.width, self.height))    
+
+    def u_d(self):
+        keys = key.get_pressed()       
+        if keys [K_w]:
+            self.rect -= self.speed
+        if keys [K_s]:
+            self.rect += self.speed     
